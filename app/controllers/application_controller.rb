6d0949a023dffee51a06e112511d6ee499777178
relaxcore <yurii.rogulya@teamvoy.com>
2024-05-16 23:23:40 +0300

    Handle user authentication by `User#auth_token`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+    user = User.find_by(auth_token: authorization_header)
+
+    head :unauthorized if authorization_header.blank? || user.blank?
+
+    Current.user = user
+  end
+
+  def authorization_header
+    @authorization_header ||= request.headers['Authorization']
+  end
 end
