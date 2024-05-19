70114318be7f9c3cb2ae576cb567dc591949525d
relaxcore <yurii.rogulya@teamvoy.com>
2024-05-19 18:55:35 +0300

    Setup CircleCI

--- db/migrate/20240516190626_create_users.rb
+++ db/migrate/20240516190626_create_users.rb
@@ -2,6 +2,9 @@
 
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
+    enable_extension 'uuid-ossp'
+    enable_extension 'pgcrypto'
+
     create_table :users, id: :uuid do |t|
       t.string :auth_token
       t.string :username
