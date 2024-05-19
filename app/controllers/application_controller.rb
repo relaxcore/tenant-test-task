d3392c99e34e06cce53d546103758f3653fac109
relaxcore <yurii.rogulya@teamvoy.com>
2024-05-19 18:03:17 +0300

    Early return when user is unauthorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::API
   def authenticate_user
     user = User.find_by(auth_token: authorization_header)
 
-    head :unauthorized if authorization_header.blank? || user.blank?
+    return head :unauthorized if authorization_header.blank? || user.blank?
 
     Current.user = user
   end
